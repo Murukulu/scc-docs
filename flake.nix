9bb173bb12beeead7223672f3dc4359cdf9e9d7c
Tom Cassar <cassar.thomas.e@gmail.com>
2025-09-13 18:47:17 +0100

    Create Nix flake for MKDocs
    - Install Python, mkdocs via nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A multi-system flake to serve docs for the SCC compiler";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.python3
+              (pkgs.python3.withPackages (ps: [
+                ps.mkdocs
+                ps.mkdocs-material
+              ]))
+            ];
+
+            shellHook = ''
+              echo "Entered SCC MKDocs development environment."
+              echo "Run 'mkdocs serve' to start the local server."
+
+              export PATH=${pkgs.zsh}/bin:$PATH
+              export SHELL=${pkgs.zsh}/bin/zsh
+              exec ${pkgs.zsh}/bin/zsh --login
+            '';
+          };
+        });
+    };
+}
+
